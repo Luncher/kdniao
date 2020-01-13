c69a67afca3c93026b9ee10407d0968830c8f2e4
Charmtiger <charmtiger@gmail.com>
2020-01-14 03:38:36 +0800

    Update release.js

--- src/config/release.js
+++ src/config/release.js
@@ -1,14 +1,14 @@
 export default {
   trace: {
     type: '1008',
-    url: 'http://api.kdniao.cc/api/dist'
+    url: 'http://api.kdniao.com/api/dist'
   },
   traceSync: {
     type: '1002',
-    url: 'http://api.kdniao.cc/Ebusiness/EbusinessOrderHandle.aspx'
+    url: 'http://api.kdniao.com/Ebusiness/EbusinessOrderHandle.aspx'
   },
   asyncSubscribe: {
     type: '101',    
-    url: 'http://api.kdniao.cc/api/dist'        
+    url: 'http://api.kdniao.com/api/dist'        
   }
 };
